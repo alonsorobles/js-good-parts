f0307138da6594b6f3406da836fefe8ed0e5099b
Alonso Robles <alonso@alonsorobles.com>
2013-01-21 14:27:57 -0600

    Method invocation and function invocation

--- program.js
+++ program.js
@@ -1,1 +1,29 @@
-document.writeln('Hello, world!');
+var add = function (a, b) {
+    return a + b;
+};
+
+var myObject = {
+    value:0,
+    increment:function (inc) {
+        this.value += typeof inc === 'number' ? inc : 1;
+    }
+};
+
+myObject.increment();
+document.writeln(myObject.value);
+
+myObject.increment(2);
+document.writeln(myObject.value);
+
+myObject.double = function () {
+    var that = this;
+
+    var helper = function () {
+        that.value = add(that.value, that.value);
+    };
+
+    helper();
+};
+
+myObject.double();
+document.writeln(myObject.value);
